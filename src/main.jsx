168287b0c7568c63d03353dac12d55b061a6084d
Raluca Nicola <RNicola@esri.com>
2022-10-21 12:04:31 +0200

    Update to latest react, jsapi, calcite

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { App } from './App';
 import { setAssetPath } from '@esri/calcite-components/dist/components';
 setAssetPath('https://unpkg.com/@esri/calcite-components/dist/calcite/assets');
 import './main.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
